372cd09c362857c1650a9772e7215104e970f5b3
Cherry-Machado <Cherry5467@gmail.com>
2025-04-07 06:45:38 -0300

    Creating HTML

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
-import './style.css'
+
+// Dynamically update the year and date of last modification
+window.onload = () => {
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+    document.getElementById(
+      "lastModified"
+    ).textContent = `Last Modification: ${document.lastModified}`;
+  };
+  
