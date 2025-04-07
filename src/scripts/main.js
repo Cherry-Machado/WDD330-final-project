0b8084369fd19fc2fbd2b8278acf7515bd6459de
Cherry-Machado <Cherry5467@gmail.com>
2025-04-07 06:57:38 -0300

    Updating

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,6 @@
-
 // Dynamically update the year and date of last modification
 window.onload = () => {
-    document.getElementById("currentyear").textContent = new Date().getFullYear();
-    document.getElementById(
-      "lastModified"
-    ).textContent = `Last Modification: ${document.lastModified}`;
-  };
-  
+  document.getElementById('currentyear').textContent = new Date().getFullYear();
+  document.getElementById('lastModified').textContent =
+    `Last Modification: ${document.lastModified}`;
+};
