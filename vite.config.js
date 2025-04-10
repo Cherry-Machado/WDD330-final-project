7c3ebb730dff8196fc750c3b986e872eb0b8f360
Cherry-Machado <Cherry5467@gmail.com>
2025-04-10 03:02:54 -0300

    Updating

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,28 @@
 // eslint-disable-next-line import/namespace
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { resolve } from 'path';
 
-export default defineConfig({
-  root: resolve(__dirname, './'),
-  publicDir: resolve(__dirname, './public'),
-  build: {
-    outDir: resolve(__dirname, './dist'),
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, './index.html'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  console.log('TMDB_API_KEY:', env.VITE_TMDB_API_KEY); // Solo para verificar que funciona
+
+  return {
+    root: resolve(__dirname, './'),
+    publicDir: resolve(__dirname, './public'),
+    build: {
+      outDir: resolve(__dirname, './dist'),
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, './index.html'),
+        },
       },
     },
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
+    server: {
+      port: 3000,
+      open: true,
+    },
+  };
 });
-//
+// // Handle theme toggle;
