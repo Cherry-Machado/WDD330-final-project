66d2dd36e5e6621fa590ea08c1ed812ffb064522
Cherry-Machado <Cherry5467@gmail.com>
2025-04-14 14:23:33 -0300

    Updating

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,15 +4,11 @@ import { setupCreateEventForm } from './modules/form-handler.js';
 //import { App1 } from './modules/app.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Inicializar la aplicación cuando el DOM esté listo
-  // const inicio = new App1();
-  //  inicio(); // Inicializar la aplicación
   const ui = new UIModule(); // Instancia del módulo de interfaz
+  const events = new EventsModule(ui); // Instancia del módulo de eventos con ui inyectado
+  events.setupEventListeners(); // Invocar su funcionalidad directamente
   ui.initTextRotation();
   ui.initParallax();
   setupCreateEventForm(); // Configurar el formulario
   ui.loadView('home-view'); // Cargar la vista inicial
-
-  const events = new EventsModule(ui); // Instancia del módulo de eventos con ui inyectado
-  events.setupEventListeners(); // Invocar su funcionalidad directamente
 });
